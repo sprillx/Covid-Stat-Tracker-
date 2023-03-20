88f256de680d70ea02053ad97478f4a638d8dbd2
brian <118747961+sprillx@users.noreply.github.com>
2023-03-20 17:41:31 -0400

    Add files via upload

---
+++ Covid Stat Tracker/covidTracker.js
@@ -0,0 +1,64 @@
+const api =  'https://disease.sh/v3/covid-19/nyt/usa';
+
+fetch(api)
+.then((response) => response.json())
+.then( data => {
+    plotData(data.filter((a ,index) => index % 7 == 0));
+})
+
+
+    function plotData(data) {
+
+         var keys = data.map(a => a.date),
+            cases = data.map( a => a.cases);
+            deaths = data.map( a => a.deaths);
+
+            columns = [keys, cases, deaths];
+
+       
+
+            keys.unshift("dates");
+            deaths.unshift("deaths");
+            cases.unshift("cases");
+
+            bb.generate({
+                    bindto: "#covid-all-us-cases-and-deaths-weekly",
+                    data: {
+                 x: "dates",
+                 columns: columns,
+                 axes: { "cases": "y", "deaths": "y2"},
+                        types: {
+                        cases: "bar"
+                }
+                 
+                    },
+                   axis: {
+                                x: {
+                                    type: "category",
+                                    clipPath: false,
+                             tick: {
+                                        count: 10,
+                                        fit: true
+                                    }
+                                },
+                        y: {
+                            label: {
+                                text: "cases",
+                                position: "outer-center"
+                            }
+                        },
+                        y2:{
+                                show: true,
+                                label: {
+                                        text: "deaths",
+                                        position: "outer-center"
+                                }
+                        },
+                        padding: { right: 100}
+                            }
+                });
+            
+                
+          
+               
+    }
